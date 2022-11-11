8308263520c19d217e77f9ff0b008b3b902d8346
Pierre Sibert <37030659+Pierronus@users.noreply.github.com>
2022-11-11 11:54:31 +0100

    Update calcul_longueur_onde_energie.py

--- calcul_longueur_onde_energie.py
+++ calcul_longueur_onde_energie.py
@@ -1,6 +1,6 @@
 def transfo(Ei, Ef):
     dE = Ef - Ei
-    varjoule = dE * 1.6 * 10**-19
+    varjoule = abs(dE) * 1.6 * 10**-19
     lambd = (((6.63*10**-34)*3.0*10**8)/varjoule)*10**9
     return "La longueur d'onde est de " + str(lambd) + " nm"
     
